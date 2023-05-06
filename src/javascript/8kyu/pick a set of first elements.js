db9acab1f244667a60117b1844e19342ca361c44
jv-vogler <jvsvogler@gmail.com>
2023-05-06 19:35:52 -0300

    Add 'pick a set of first elements'

---
+++ src/javascript/8kyu/pick a set of first elements.js
@@ -0,0 +1,26 @@
+/* 
+Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+*/
+
+function first(arr, n) {
+  switch (n) {
+    case 0:
+      return []
+      break
+    case undefined:
+      return [arr[0]]
+      break
+    default:
+      return arr.splice(0, n)
+      break
+  }
+}
